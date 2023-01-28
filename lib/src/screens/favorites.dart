4c30285e93e6cd9053c7a915027019348cf9c713
Flyxe <antoine.hourdeau1@gmail.com>
2023-01-28 16:23:53 +0100

    création screens & navigation
    Ajout du système de navigation entre le screen Home et le screen Favoris

---
+++ lib/src/screens/favorites.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavoritesPage extends StatelessWidget {
+  static const routeName = '/FavoritesPage';
+
+  const FavoritesPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Text("page favoris");
+
+  }
+}
---
+++ lib/src/screens/navigation.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:test_hello_cse/src/screens/favorites.dart';
+
+import 'home.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    FavoritesPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_rounded),
+            label: 'Films',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoris',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
